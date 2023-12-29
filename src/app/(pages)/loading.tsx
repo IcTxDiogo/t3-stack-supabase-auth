b7753b76d25cf4c3f21c060622cacb03dd2a4b0d
Diogo frança dos santos <ictxdiogo@gmail.com>
2023-12-29 11:05:07 -0400

    feat: moved async getLoggedUserData to own component and use suspense and loading

---
+++ src/app/(pages)/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <>
+      <main className={"flex min-h-screen items-center justify-center"}>
+        <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-gray-900"></div>
+      </main>
+    </>
+  );
+}
